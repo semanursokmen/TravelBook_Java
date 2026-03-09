90cf73bb44304a75860d34590303195817d7ea13
semanursokmen <126008451+semanursokmen@users.noreply.github.com>
2026-03-09 13:18:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/javatravelbook/roomdb/PlaceDatabase.java
@@ -0,0 +1,12 @@
+package com.example.javatravelbook.roomdb;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.javatravelbook.model.Place;
+
+@Database(entities = {Place.class}, version=1)
+public abstract class PlaceDatabase extends RoomDatabase {
+    public abstract PlaceDao placeDao();
+
+}
